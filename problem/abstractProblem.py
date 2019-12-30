6cec84f55c222c0dcb283c22ea655a1209b77c1e
Marcin Bołba <marcin.bolba@wp.pl>
2019-12-30 18:48:36 +0100

    Remove genotype length from problem definition

--- problem/abstractProblem.py
+++ problem/abstractProblem.py
@@ -2,9 +2,6 @@ from abc import ABC
 
 
 class Problem:
-    def genotypeLenght(self):
-        raise NotImplementedError("The method not implemented")
-
     def goalFunction(self, value):
         raise NotImplementedError("The method not implemented")
 
--- problem/x2.py
+++ problem/x2.py
@@ -4,11 +4,8 @@ from problem.abstractProblem import Problem
 
 
 class X2(Problem):
-    def genotypeLenght(self):
-        return 6
-
     def goalFunction(self, value):
         return np.power(value, 2)
 
     def adaptationFunction(self, value):
-        return np.power(value,2)
+        return np.power(value, 2)
