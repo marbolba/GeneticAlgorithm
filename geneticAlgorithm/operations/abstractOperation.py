6ca2584d5c4205cccaa5c7331ceb1e8f8650e0f7
Marcin Bołba <marcin.bolba@wp.pl>
2020-03-01 15:38:15 +0100

    Add POC version of elite succession

--- geneticAlgorithm/operations/abstractOperation.py
+++ geneticAlgorithm/operations/abstractOperation.py
@@ -20,11 +20,20 @@ class Operation:
     # SUCCESSION
     @staticmethod
     def trivialSuccession(population: Population, newPopulation: [Individual]):
-        print("setting new:",newPopulation)
         population.setPopulation(newPopulation)
 
     @staticmethod
     def eliteSuccession(population: Population, newPopulation: [Individual]):
         sortedIndividuals = sorted(population.population, key=lambda x: x._adaptation, reverse=True)
-        # newPopulation[0] = sortedIndividuals[0]
-        # population = newPopulation
+        newPopulation[0] = sortedIndividuals[0]
+        newPopulation[3] = sortedIndividuals[1]
+        newPopulation[5] = sortedIndividuals[2]
+        newPopulation[7] = sortedIndividuals[3]
+        newPopulation[9] = sortedIndividuals[4]
+        newPopulation[11] = sortedIndividuals[5]
+        newPopulation[13] = sortedIndividuals[6]
+        newPopulation[15] = sortedIndividuals[7]
+        newPopulation[17] = sortedIndividuals[8]
+        newPopulation[19] = sortedIndividuals[9]
+        newPopulation[21] = sortedIndividuals[10]
+        population.setPopulation(newPopulation)
