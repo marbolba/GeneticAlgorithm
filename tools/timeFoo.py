fae6ff2c1c3e8367e20689836c72828a91244fef
Marcin Bołba <marcin.bolba@wp.pl>
2020-04-11 22:06:54 +0200

    Add function measuring decorator

---
+++ tools/timeFoo.py
@@ -0,0 +1,10 @@
+import time
+
+def timeFoo(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        print('--- executed %r in %2.2f s ---' % (method.__name__, (te - ts)))
+        return result
+    return timed
