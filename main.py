5d163c1578df70683875684754afe2c97262b18f
Marcin Bołba <marcin.bolba@wp.pl>
2020-05-10 15:21:06 +0200

    Add terrain sel to main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from geneticAlgorithm.ga import Ga
 from problem.terrainProblem import TerrainProblem
 from problem.terrainProblemStep import TerrainProblemStep
+from problem.terrainProblemSel import TerrainProblemSel
 from problem.x2Problem import X2Problem
 from settings.terrainSettings import TerrainSetting
 from settings.terrainSettingsStep import TerrainSettingStep
+from settings.terrainSettingsSel import TerrainSettingSel
 from settings.x2Setting import X2Setting
 from tools.terrainHandler import TerrainHandler
 
@@ -12,6 +14,14 @@ def runForX2():
     ga = Ga()
     ga.setSetting(X2Setting())
     ga.setProblem(X2Problem())
+    ga.initPopulation()
+
+    ga.startAlgorithm()
+
+def runForTerrainSel():
+    ga = Ga()
+    ga.setSetting(TerrainSettingSel())
+    ga.setProblem(TerrainProblemSel())
     ga.initPopulation()
 
     ga.startAlgorithm()
@@ -36,4 +46,4 @@ def runForTerrainStep():
 
 if __name__ == "__main__":
     TerrainHandler.setName("case2")
-    runForTerrain()
+    runForTerrainSel()
