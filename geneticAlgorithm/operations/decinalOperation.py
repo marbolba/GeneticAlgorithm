e731ba19db4f877f62ee1453cf26f32408cd0202
Marcin Bołba <marcin.bolba@wp.pl>
2020-09-02 22:53:42 +0200

    Add double point crossover

--- geneticAlgorithm/operations/decinalOperation.py
+++ geneticAlgorithm/operations/decinalOperation.py
@@ -25,18 +25,45 @@ class DecimalOperation(Operation):
                 print(
                     "ERR: cannot perform crossover (index out of bound {}".format(i + 1)
                 )
+                return
 
             if np.random.rand() <= setting.crossoverProbability():
-                tmpGenotype1 = population[i].genotype.genotype  # ???
-                tmpGenotype2 = population[i + 1].genotype.genotype  # ???
+                tmpGenotype1 = population[i].genotype.genotype  
+                tmpGenotype2 = population[i + 1].genotype.genotype  
                 # randomly select cut point
                 cutPoint = np.random.randint(1, genotypeLength)
                 # print('cx:', population[i].genotype.genotype, '&', population[i + 1].genotype.genotype, 'in', cutPoint)
                 for j in range(cutPoint, genotypeLength):
                     tmpGenotype1[j], tmpGenotype2[j] = tmpGenotype2[j], tmpGenotype1[j]
-                population[i].setGenotype(tmpGenotype1)  # ???
-                population[i + 1].setGenotype(tmpGenotype2)  # ???
+                population[i].setGenotype(tmpGenotype1)  
+                population[i + 1].setGenotype(tmpGenotype2)  
                 # print('ax:', population[i].genotype.genotype, '&', population[i + 1].genotype.genotype, 'in', cutPoint)
+                
+    @staticmethod
+    def doublePointCrossover(population: [Individual], setting: Setting):
+        sizeOfPopulation = len(population)
+        genotypeLength = len(population[0].genotype.genotype)
+        for i in range(0, sizeOfPopulation, 2):
+            # guard block
+            if not i + 1 < sizeOfPopulation:
+                print(
+                    "ERR: cannot perform crossover (index out of bound {}".format(i + 1)
+                )
+
+            if np.random.rand() <= setting.crossoverProbability():
+                tmpGenotype1 = population[i].genotype.genotype  
+                tmpGenotype2 = population[i + 1].genotype.genotype  
+                # randomly select cut point
+                cutPoint1 = np.random.randint(1, genotypeLength)
+                cutPoint2 = np.random.randint(1, genotypeLength)
+                # print('cx:', population[i].genotype.genotype, '&', population[i + 1].genotype.genotype, 'in', cutPoint1, '&', cutPoint2)
+                for j in range(cutPoint1, genotypeLength):
+                    tmpGenotype1[j], tmpGenotype2[j] = tmpGenotype2[j], tmpGenotype1[j]
+                for j in range(cutPoint2, genotypeLength):
+                    tmpGenotype1[j], tmpGenotype2[j] = tmpGenotype2[j], tmpGenotype1[j]
+                population[i].setGenotype(tmpGenotype1)  
+                population[i + 1].setGenotype(tmpGenotype2)  
+                # print('ax:', population[i].genotype.genotype, '&', population[i + 1].genotype.genotype, 'in', cutPoint1, '&', cutPoint2)
 
     # MUTATION
     @staticmethod
