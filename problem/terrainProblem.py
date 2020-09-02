6621fd5d419291ed4c246164d39de8faacfc24f2
Marcin Bołba <marcin.bolba@wp.pl>
2020-09-02 22:53:20 +0200

    Update adaptation function

--- problem/terrainProblem.py
+++ problem/terrainProblem.py
@@ -14,31 +14,44 @@ class TerrainProblem(Problem):
         return points
 
     def adaptationFunction(self, values: []):
-        costFunction = lambda x: 187.5 * (pow(1.028, x * 100))
-        # settings
+    # settings
+        # costFunction: always between <0,1>
+        # costFunction = lambda x: x # linear
+        # costFunction = lambda x: (1/16)*pow(16,x) # exponential (0.75,0.5)
+        costFunction = lambda x: (1/64)*pow(64,x) # exponential (0.83,0.5)
+        # factor / ratio:
+        sum = 1000
+        W = 5/3
+        b = sum / (W + 1)
+        a = sum - b
+
+    # the rest
+
         startPoint = TerrainHandler.getWaypoints()[0]
         endPoint = TerrainHandler.getWaypoints()[1]
 
-        distanceMax = TerrainHandler.distance((0,0),TerrainHandler.getSize())
+        distanceMax = TerrainHandler.distance((0, 0), TerrainHandler.getSize())
         costMax = 10000
 
-        # sprawdzenie czy sa punkty do przejscia
+        # check if there are any points on the road
         valuesLoc = values.copy()
         if len(valuesLoc) < 1:
             return 0
 
-        # składowa dystansu - ostatni element do endpoint
-        distanceElement = TerrainHandler.distance(
-            (valuesLoc[len(valuesLoc) - 1][0], valuesLoc[len(valuesLoc) - 1][1]), endPoint
+        # calculate distance 
+        distance = TerrainHandler.distance(
+            valuesLoc[len(valuesLoc) - 1],
+            endPoint,
         )
 
-        # skladowa kosztu podróży
+        # calculate cost
         cost = 0
         valuesLoc.insert(0, startPoint)
         for i in range(0, len(valuesLoc) - 1):
             cost = cost + TerrainHandler.travelCost(valuesLoc[i], valuesLoc[i + 1])
 
-        adaptationVal = 5000 * (
-            distanceMax - distanceElement
-        ) / distanceMax + costFunction((costMax - cost) / costMax)
+        distance_element = (distanceMax - distance) / distanceMax
+        cost_element = (costMax - cost) / costMax
+
+        adaptationVal = a * distance_element + b * costFunction(cost_element)
         return adaptationVal if adaptationVal > 0 else 0
